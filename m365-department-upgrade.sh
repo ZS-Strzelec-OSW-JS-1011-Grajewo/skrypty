fe048806dc6b3d1ebf5e4dff3429f05d1aafa0c9
Damian Fajfer <damian@fajfer.org>
2023-08-31 21:58:53 +0200

    Refactor, ulepszony standard skryptu

--- m365-department-upgrade.sh
+++ m365-department-upgrade.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 ## Reqs:
-# az cli
 # m365-login.sh executed
 # jq
 
-# zmienić --filter w linii 10
-# zmienić --department w linii 18
+department="Sekcja Instruktorów"
 
-ids=( $(az ad user list --filter "department eq 'Pluton 2 Sekc. Instr.'" | jq '.[] | .id, .givenName, .surname' | sed -n '1~3p' | sed 's/"//g') )
-length=${#ids[@]}
+TOKENS="${PWD}/.cli-m365-tokens.json"
+MSAL="${PWD}/.cli-m365-msal.json"
+
+ids=( $(docker run \
+  --mount type=bind,source="${TOKENS}",dst=/home/cli-microsoft365/.cli-m365-tokens.json \
+  --mount type=bind,source="${MSAL}",dst=/home/cli-microsoft365/.cli-m365-msal.json \
+  m365pnp/cli-microsoft365 \
+  m365 aad user list --department "${department}" --properties "id" | jq -r '.[] | .id') )
 
 for (( i = 0; i < length; i++ ))
 do
     docker run \
-        --mount type=bind,source="${PWD}"/.cli-m365-tokens.json,dst=/home/cli-microsoft365/.cli-m365-tokens.json \
-        --mount type=bind,source="${PWD}"/.cli-m365-msal.json,dst=/home/cli-microsoft365/.cli-m365-msal.json \
-        m365pnp/cli-microsoft365 m365 aad user set --objectId "${ids[i]}" --department "Sekcja Instruktorów"
+        --mount type=bind,source="${TOKENS}",dst=/home/cli-microsoft365/.cli-m365-tokens.json \
+        --mount type=bind,source="${MSAL}",dst=/home/cli-microsoft365/.cli-m365-msal.json \
+        m365pnp/cli-microsoft365 \
+        m365 aad user set --objectId "${ids[i]}" --department "${department}"
 done
