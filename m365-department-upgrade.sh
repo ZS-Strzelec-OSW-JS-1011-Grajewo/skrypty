1a36a595c290c5b61ca9f46047d2c225d1cf8f6b
Damian Fajfer <damian@fajfer.org>
2023-08-31 22:26:07 +0200

    Kolejny refactor nowego formatu skryptu

--- m365-department-upgrade.sh
+++ m365-department-upgrade.sh
@@ -5,20 +5,15 @@
 
 department="Sekcja Instruktorów"
 
-TOKENS="${PWD}/.cli-m365-tokens.json"
-MSAL="${PWD}/.cli-m365-msal.json"
+DKR="docker run \
+        --mount type=bind,source="${PWD}"/.cli-m365-tokens.json,dst=/home/cli-microsoft365/.cli-m365-tokens.json \
+        --mount type=bind,source="${PWD}"/.cli-m365-msal.json,dst=/home/cli-microsoft365/.cli-m365-msal.json \
+        m365pnp/cli-microsoft365"
 
-ids=( $(docker run \
-  --mount type=bind,source="${TOKENS}",dst=/home/cli-microsoft365/.cli-m365-tokens.json \
-  --mount type=bind,source="${MSAL}",dst=/home/cli-microsoft365/.cli-m365-msal.json \
-  m365pnp/cli-microsoft365 \
-  m365 aad user list --department "${department}" --properties "id" | jq -r '.[] | .id') )
+ids=( $(${DKR} m365 aad user list --department "${department}" --properties "id" | jq -r '.[] | .id') )
+length=${#ids[@]}
 
 for (( i = 0; i < length; i++ ))
 do
-    docker run \
-        --mount type=bind,source="${TOKENS}",dst=/home/cli-microsoft365/.cli-m365-tokens.json \
-        --mount type=bind,source="${MSAL}",dst=/home/cli-microsoft365/.cli-m365-msal.json \
-        m365pnp/cli-microsoft365 \
-        m365 aad user set --objectId "${ids[i]}" --department "${department}"
+    ${DKR} m365 aad user set --objectId "${ids[i]}" --department "${department}"
 done
